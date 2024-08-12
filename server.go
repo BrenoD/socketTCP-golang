78bebc41ad634bda664de8ff99bd4b54421e546d
BrenoD <dias2579@gmail.com>
2024-08-12 15:32:56 -0300

    added client msg

--- server.go
+++ server.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net"
-	"strings"
 )
 
 const (
@@ -13,12 +11,7 @@ const (
 	TCP_PORT    = "8080"
 )
 
-type Message struct {
-	Message string `json:"message"`
-}
-
 func main() {
-
 	go startTCPServer()
 
 	listener, err := net.Listen("tcp", SERVER_HOST+":"+SERVER_PORT)
@@ -48,7 +41,7 @@ func startTCPServer() {
 	}
 	defer listener.Close()
 
-	fmt.Println("Servidor TCP simulando HTTP ouvindo em localhost:" + TCP_PORT)
+	fmt.Println("Servidor TCP ouvindo em", SERVER_HOST+":"+TCP_PORT)
 
 	for {
 		conn, err := listener.Accept()
@@ -57,11 +50,34 @@ func startTCPServer() {
 			continue
 		}
 
-		go handleHTTPRequest(conn)
+		go handleTCPConnection(conn)
 	}
 }
 
-func handleHTTPRequest(conn net.Conn) {
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Erro ao ler dados:", err)
+		return
+	}
+
+	fmt.Println("Recebido:", string(buffer[:n]))
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 23\r\n" +
+		"\r\n" +
+		"Mensagem recebida via TCP"
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Erro ao enviar dados:", err)
+	}
+}
+
+func handleTCPConnection(conn net.Conn) {
 	defer conn.Close()
 
 	buffer := make([]byte, 1024)
@@ -72,72 +88,16 @@ func handleHTTPRequest(conn net.Conn) {
 	}
 
 	data := string(buffer[:n])
-	lines := strings.Split(data, "\r\n")
-	if len(lines) < 2 {
-		fmt.Println("Requisição inválida")
-		return
-	}
-
-	body := lines[len(lines)-1]
-
-	// parsing do JSON
-	var msg Message
-	err = json.Unmarshal([]byte(body), &msg)
-	if err != nil {
-		fmt.Println("Erro ao fazer parse do JSON:", err)
-		return
-	}
-
-	response, err := sendToTCPServer(msg.Message)
-	if err != nil {
-		fmt.Println("Erro ao enviar dados para o servidor TCP:", err)
-		return
-	}
+	fmt.Println("Recebido no TCP:", data)
 
 	httpResponse := "HTTP/1.1 200 OK\r\n" +
 		"Content-Type: text/plain\r\n" +
-		"Content-Length: " + fmt.Sprintf("%d", len(response)) + "\r\n" +
+		"Content-Length: " + fmt.Sprintf("%d", len(data)) + "\r\n" +
 		"\r\n" +
-		response
-	conn.Write([]byte(httpResponse))
-}
+		data
 
-func sendToTCPServer(message string) (string, error) {
-	conn, err := net.Dial("tcp", SERVER_HOST+":"+SERVER_PORT)
+	_, err = conn.Write([]byte(httpResponse))
 	if err != nil {
-		return "", err
-	}
-	defer conn.Close()
-
-	_, err = conn.Write([]byte(message))
-	if err != nil {
-		return "", err
-	}
-
-	buffer := make([]byte, 1024)
-	mLen, err := conn.Read(buffer)
-	if err != nil {
-		return "", err
-	}
-
-	return string(buffer[:mLen]), nil
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	buffer := make([]byte, 1024)
-	mLen, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Erro ao ler dados:", err)
-		return
-	}
-
-	fmt.Println("Recebido:", string(buffer[:mLen]))
-
-	// resposta de volta ao cliente
-	_, err = conn.Write([]byte("Mensagem recebida via TCP"))
-	if err != nil {
-		fmt.Println("Erro ao enviar dados:", err)
+		fmt.Println("Erro ao enviar resposta HTTP:", err)
 	}
 }
